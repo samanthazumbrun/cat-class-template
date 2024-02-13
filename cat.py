dff62b8e43bc27a416b743d368be13d3a145b935
samanthazumbrun <samantha.zumbrun@students.fhu.edu>
2024-02-13 12:29:29 -0600

    cat.py
    added OOP and def

--- cat.py
+++ cat.py
@@ -1,6 +1,22 @@
 #Revmoe pass and complete the cat class
 class Cat():
-    pass
+    def __init__(self):
+        self.name = "Unknown"
+        self.age = 0
+    def speak(self):
+        return 'Meow'
+    
+stella = Cat()
+stella.name = "Stella"
+stella.age = 7
+stella.speak
+
+garfield = Cat()
+garfield.name = "Garfield"
+garfield.age = 50
+garfield.speak
+
+    
 
 
 #Create objects here
